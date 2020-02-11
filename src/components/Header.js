eb2c2ebb7cdcc7578849fd11ec7374963dcc5764
foucher valentin <valentin.foucher@payfit.com>
2020-02-11 17:16:26 +0100

    inti project 2

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,19 +3,10 @@ import React from 'react'
 
 const Header = props => (
   <header id="header" style={props.timeout ? { display: 'none' } : {}}>
-    <div className="logo">
-      <span className="icon fa-diamond"></span>
-    </div>
     <div className="content">
       <div className="inner">
-        <h1>Dimension</h1>
-        <p>
-          A fully responsive site template designed by{' '}
-          <a href="https://html5up.net">HTML5 UP</a> and released
-          <br />
-          for free under the{' '}
-          <a href="https://html5up.net/license">Creative Commons</a> license.
-        </p>
+        <h1>MINI 990 Valentin Foucher</h1>
+        <p></p>
       </div>
     </div>
     <nav>
