47d2499ab1358e42baa159058f62b101414084a5
foucher valentin <valentin.foucher@payfit.com>
2020-02-24 16:58:25 +0100

    added robots.txt, sitemap and removed footer

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,11 @@ module.exports = {
   siteMetadata: {
     title: 'Mini 990 Valentin Foucher',
     author: 'Valentin Foucher',
-    description: 'A Gatsby.js V2 Starter based on Dimension by HTML5 UP',
+    description:
+      'Voici le site de mon projet mini 6.50, en vue la la mini transat 2021',
     titleTemplate: 'Mini 990 Valentin Foucher',
-    url: 'www.mini990valentinfoucher.netlify.com',
+    siteUrl: 'https://www.mini990valentinfoucher.netlify.com',
+    url: 'https://www.mini990valentinfoucher.netlify.com',
     image: 'src/images/logo.png',
   },
   plugins: [
@@ -19,6 +21,37 @@ module.exports = {
         theme_color: '#663399',
         display: 'minimal-ui',
         icon: 'src/images/logo.png', // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        host: 'https://mini990valentinfoucher.netlify.com/',
+        sitemap: 'https://mini990valentinfoucher.netlify.com/sitemap.xml',
+        env: {
+          development: {
+            policy: [
+              {
+                userAgent: '*',
+                disallow: ['/'],
+              },
+            ],
+          },
+          production: {
+            policy: [
+              {
+                userAgent: '*',
+                allow: '/',
+              },
+            ],
+          },
+        },
+      },
+    },
+    {
+      resolve: `gatsby-plugin-sitemap`,
+      options: {
+        exclude: [`/`],
       },
     },
     'gatsby-plugin-sass',
