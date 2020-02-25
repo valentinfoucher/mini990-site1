f37ec1e04da08ee0f841a942bae2de8a81e59f06
foucher valentin <valentin.foucher@payfit.com>
2020-02-25 17:46:28 +0100

    added tracking id

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,8 +55,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        // replace "UA-XXXXXXXXX-X" with your own Tracking ID
-        trackingId: "UA-XXXXXXXXX-X",
+        trackingId: "UA-159035307-1",
       },
     },
   ],
